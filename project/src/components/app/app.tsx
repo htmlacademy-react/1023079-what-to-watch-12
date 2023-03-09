87af2c6540d259dc7002cd803250aa75e4c5e526
a.svirin <a.svirin94@gmail.com>
2023-03-09 20:02:33 +0400

    feat: Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../consts';
+import AddReview from '../../pages/add-review/AddReview';
+import FilmPage from '../../pages/film/film';
 import MainPage from '../../pages/main/main';
+import MyList from '../../pages/my-list/myList';
+import NotFound from '../../pages/not-found/notFound';
+import Player from '../../pages/player/player';
+import SignIn from '../../pages/sign-in/signIn';
 
 type FilmIds = {
   id: number;
@@ -13,11 +21,44 @@ type AppScreenProps = {
 
 export default function App({title, genre, year, filmIds}: AppScreenProps): JSX.Element {
   return (
-    <MainPage
-      title={title}
-      genre={genre}
-      year={year}
-      filmIds={filmIds}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = {AppRoute.Main}
+          element = {
+            <MainPage
+              title={title}
+              genre={genre}
+              year={year}
+              filmIds={filmIds}
+            />
+          }
+        />
+        <Route
+          path = {AppRoute.SignIn}
+          element = {<SignIn />}
+        />
+        <Route
+          path = {AppRoute.MyList}
+          element = {<MyList />}
+        />
+        <Route
+          path = {AppRoute.Film}
+          element = {<FilmPage />}
+        />
+        <Route
+          path = {AppRoute.AddReview}
+          element = {<AddReview />}
+        />
+        <Route
+          path = {AppRoute.Player}
+          element = {<Player />}
+        />
+        <Route
+          path = '*'
+          element = {<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ project/src/consts.ts
@@ -0,0 +1,8 @@
+export const AppRoute = {
+  Main: '/',
+  SignIn: '/login',
+  MyList: '/mylist',
+  Film: '/films/:id',
+  AddReview: '/films/:id/review',
+  Player: '/player/:id'
+} as const;

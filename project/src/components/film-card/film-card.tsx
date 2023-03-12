c01b808ae131adb962340df2a7a0071f1ed34444
a.svirin <a.svirin94@gmail.com>
2023-03-12 17:34:23 +0400

    fix film-card

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -4,14 +4,14 @@ import { FilmType } from '../../types/film-type';
 type Props = Pick<FilmType, 'name' | 'posterImage'>
 
 export default function FilmCard(props: Props): JSX.Element{
-  const {name, posterImage} = props;
+
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={posterImage} alt={name} width="280" height="175" />
+        <img src={props.posterImage} alt={props.name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{name}</a>
+        <a className="small-film-card__link" href="film-page.html">{props.name}</a>
       </h3>
     </article>
   );

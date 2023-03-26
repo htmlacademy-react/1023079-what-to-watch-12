c68361acec1f8737614cbf09a3b0c0036cb75c1d
a.svirin <a.svirin94@gmail.com>
2023-03-26 23:00:48 +0400

    fix: Передает на страницы только нужный фильм

--- project/src/pages/add-review/add-review.tsx
+++ project/src/pages/add-review/add-review.tsx
@@ -1,17 +1,15 @@
 import { Helmet } from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
 import { Film } from '../../types/film-type';
-import {useParams} from 'react-router-dom';
 import Review from '../../components/add-review/review';
+import useGetFilmOnPage from '../../hooks/useGetFilmOnPage';
 
 type Props = {
   films: Film[];
 }
 
 export default function AddReview(props: Props): JSX.Element {
-  const paramId = useParams().id;
-  const films = props.films;
-  const filmForReview = films.find((film) => film.id === Number(paramId));
+  const film = useGetFilmOnPage(props.films) as Film;
 
   return (
     <section className="film-card film-card--full">
@@ -20,7 +18,7 @@ export default function AddReview(props: Props): JSX.Element {
       </Helmet>
       <div className="film-card__header">
         <div className="film-card__bg">
-          <img src={filmForReview?.backgroundImage} alt={filmForReview?.name}/>
+          <img src={film.backgroundImage} alt={film.name}/>
         </div>
         <h1 className="visually-hidden">WTW</h1>
         <header className="page-header">
@@ -28,7 +26,7 @@ export default function AddReview(props: Props): JSX.Element {
           <nav className="breadcrumbs">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
-                <a href="film-page.html" className="breadcrumbs__link">{filmForReview?.name}</a>
+                <a href="film-page.html" className="breadcrumbs__link">{film.name}</a>
               </li>
               <li className="breadcrumbs__item">
                 <a className="breadcrumbs__link">Add review</a>
@@ -47,7 +45,7 @@ export default function AddReview(props: Props): JSX.Element {
           </ul>
         </header>
         <div className="film-card__poster film-card__poster--small">
-          <img src={filmForReview?.posterImage} alt={filmForReview?.name} width="218" height="327"/>
+          <img src={film.posterImage} alt={film.name} width="218" height="327"/>
         </div>
       </div>
       <Review />
